d2eab04b276f20807dc7cb71bc97a44911de0706
dnov09 <dngige9@gmail.com>
2019-11-06 16:18:00 -0600

    config file + gitignore updated

--- public/script.js
+++ public/script.js
@@ -1,18 +1,7 @@
+import * as config from "./.config.js";
+
 (function(){
     var flag=0;
-
-    const firebaseConfig = {
-        apiKey: "AIzaSyCwEjL5VKMwbK_Zz5X4AYTZPYoOgkZP1k8",
-        authDomain: "database-20325.firebaseapp.com",
-        databaseURL: "https://database-20325.firebaseio.com",
-        projectId: "database-20325",
-        storageBucket: "database-20325.appspot.com",
-        messagingSenderId: "698450547042",
-        appId: "1:698450547042:web:04867510b1a497c6689c3b",
-        measurementId: "G-E4M8SMQP0V"
-      };
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
 
 document.getElementById("sign").onclick= function (){
     const email=document.getElementById("email").value;
