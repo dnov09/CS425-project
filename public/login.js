8a86d8b5abf2c8e062b37d20076d0b347cc90c92
Ayobami Adebowale <blesi@hawk.iit.edu>
2019-11-10 16:26:09 -0600

    made changes

--- public/login.js
+++ public/login.js
@@ -7,17 +7,8 @@ import * as config from "./.config.js";
         const password=document.getElementById("pswd").value;
         
     
-        const auth = firebase.auth();
-        const promise = auth.signInWithEmailAndPassword(email,password);
-        promise.catch(e=> console.log(e.message));
-       // location.replace("profile.html");
-       firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-          alert("oshey you entered");
-        } else {
-          alert("wrong username and password");
-        }
-      });
+        
+  
     }
     
     
